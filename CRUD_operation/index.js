563cecad8972a84951cb8cec43b1fe83ac2a8a4c
palashyerpude <128513490+palashyerpude@users.noreply.github.com>
2023-03-30 17:24:57 +0530

    CRUD Opertaion
    NodeJs,MOngodb

--- CRUD_operation/index.js
+++ CRUD_operation/index.js
@@ -1,1 +1,16 @@
+const express= require('express')
+const userRouter=require('./router/user')
+const multer = require('multer');
+//const User = require('./model/user')
 
+require('./db/mongoose')
+const app=express()
+
+const PORT = process.env.PORT||3000
+app.use(express.json());
+app.use(multer({ dest: './uploads' }));
+app.use(userRouter)
+
+app.listen(PORT,()=>{
+    console.log(`server is connection on ${PORT}`)
+})
